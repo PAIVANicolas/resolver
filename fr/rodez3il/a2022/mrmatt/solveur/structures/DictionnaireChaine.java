37afce1fb7f16646b6253bc4d405065810637a03
PAIVANicolas-3il <75265504+PAIVANicolas@users.noreply.github.com>
2023-02-20 15:28:57 +0100

    add

--- fr/rodez3il/a2022/mrmatt/solveur/structures/DictionnaireChaine.java
+++ fr/rodez3il/a2022/mrmatt/solveur/structures/DictionnaireChaine.java
@@ -1,5 +1,103 @@
 package fr.rodez3il.a2022.mrmatt.solveur.structures;
 
+import java.util.NoSuchElementException;
+
 public class DictionnaireChaine<C, V> implements Dictionnaire<C, V> {
-	
+
+    private Liste<Entree<C, V>> entrees;
+
+    /**
+     * Classe interne représentant une entrée du dictionnaire (couple clé, valeur).
+     */
+    private static class Entree<C, V> {
+        private C cle;
+        private V valeur;
+
+        public Entree(C cle, V valeur) {
+            this.cle = cle;
+            this.valeur = valeur;
+        }
+
+        public C getCle() {
+            return cle;
+        }
+
+        public V getValeur() {
+            return valeur;
+        }
+
+        public void setValeur(V valeur) {
+            this.valeur = valeur;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Entree<?, ?> entree = (Entree<?, ?>) o;
+            return cle.equals(entree.cle);
+        }
+
+        @Override
+        public int hashCode() {
+            return cle.hashCode();
+        }
+    }
+
+    public DictionnaireChaine() {
+        entrees = new ListeChainee<>();
+    }
+
+    /**
+     * Insère une nouvelle entrée dans le dictionnaire, en remplaçant la valeur associée à la clé
+     * si elle existe déjà.
+     *
+     * @param cle    la clé associée à la valeur
+     * @param valeur la valeur associée à la clé
+     */
+    public void inserer(C cle, V valeur) {
+        Entree<C, V> nouvelleEntree = new Entree<>(cle, valeur);
+
+        // Si la clé existe déjà, on remplace la valeur associée
+        boolean trouve = false;
+        int i = 0;
+        while (!trouve && i < entrees.taille()) {
+            Entree<C, V> entreeExistante = entrees.element(i);
+            if (entreeExistante.getCle().equals(cle)) {
+                entreeExistante.setValeur(valeur);
+                trouve = true;
+            }
+            i++;
+        }
+
+        // Sinon, on ajoute une nouvelle entrée
+        if (!trouve) {
+            entrees.ajouter(nouvelleEntree);
+        }
+    }
+
+
+    /**
+     * Vérifie si le dictionnaire contient une entrée associée à la clé spécifiée.
+     *
+     * @param cle la clé à rechercher dans le dictionnaire
+     * @return true si le dictionnaire contient la clé, false sinon
+     */
+    public boolean contient(C cle) {
+        Entree<C, V> recherche = new Entree<>(cle, null);
+        return entrees.contient(recherche);
+    }
+
+    /**
+     * Récupère la valeur associée à la clé spécifiée.
+     *
+     * @param cle la clé associée à la valeur à récupérer
+     * @return la valeur associée à la clé
+     * @throws NoSuchElementException si la clé n'existe pas dans le dictionnaire
+     */
+    public V valeur(C cle) throws NoSuchElementException {
+
+
+        throw new NoSuchElementException("La clé " + cle + " n'existe pas dans le dictionnaire.");
+    }
 }
--- fr/rodez3il/a2022/mrmatt/solveur/structures/ListeChainee.java
+++ fr/rodez3il/a2022/mrmatt/solveur/structures/ListeChainee.java
@@ -2,4 +2,117 @@ package fr.rodez3il.a2022.mrmatt.solveur.structures;
 
 public class ListeChainee<T> implements Liste<T> {
 
+    private class Maillon {
+        private T donnee;
+        private Maillon suivant;
+
+        public Maillon(T donnee) {
+            this.donnee = donnee;
+            this.suivant = null;
+        }
+    }
+
+    private Maillon tete;
+    private int taille;
+
+    public ListeChainee() {
+        tete = null;
+        taille = 0;
+    }
+
+    /**
+     * Ajoute un élément à la fin de la liste.
+     *
+     * @param element l'élément à ajouter
+     */
+    public void ajouter(T element) {
+        Maillon nouveauMaillon = new Maillon(element);
+        if (tete == null) {
+            tete = nouveauMaillon;
+        } else {
+            Maillon dernierMaillon = tete;
+            while (dernierMaillon.suivant != null) {
+                dernierMaillon = dernierMaillon.suivant;
+            }
+            dernierMaillon.suivant = nouveauMaillon;
+        }
+        taille++;
+    }
+
+    /**
+     * Vérifie si la liste est vide.
+     * @return true si la liste est vide, false sinon.
+     **/
+    public boolean estVide() {
+        return tete == null;
+    }
+
+    /**
+     * Renvoie le nombre d'éléments dans la liste.
+     * @return la taille de la liste
+     **/
+    public int taille() {
+        return taille;
+    }
+
+    /**
+     * Enlève l'élément situé à l'indice spécifié dans cette liste.
+     * Décale les éléments suivants pour combler le vide laissé par l'élément supprimé.
+     *
+     * @param i l'indice de l'élément à enlever
+     * @return l'élément enlevé de la liste
+     * @throws IndexOutOfBoundsException si l'indice est hors limites (i < 0 || i >= taille())
+     */
+    public T enlever(int i) {
+        if (i < 0 || i >= taille) {
+            throw new IndexOutOfBoundsException();
+        }
+        T elementEnleve;
+        if (i == 0) {
+            elementEnleve = tete.donnee;
+            tete = tete.suivant;
+        } else {
+            Maillon maillonPrecedent = tete;
+            for (int j = 0; j < i - 1; j++) {
+                maillonPrecedent = maillonPrecedent.suivant;
+            }
+            elementEnleve = maillonPrecedent.suivant.donnee;
+            maillonPrecedent.suivant = maillonPrecedent.suivant.suivant;
+        }
+        taille--;
+        return elementEnleve;
+    }
+
+    /**
+     * Retourne l'élément à l'index spécifié dans la liste chaînée.
+     * @param i l'index de l'élément à récupérer (0-indexé)
+     * @return l'élément à l'index spécifié dans la liste chaînée
+     * @throws IndexOutOfBoundsException si l'index est hors de la plage valide (0 <= index < taille())
+     **/
+    public T element(int i) {
+        if (i < 0 || i >= taille) {
+            throw new IndexOutOfBoundsException();
+        }
+        Maillon maillonCourant = tete;
+        for (int j = 0; j < i; j++) {
+            maillonCourant = maillonCourant.suivant;
+        }
+        return maillonCourant.donnee;
+    }
+
+    /**
+     * Retourne vrai si l'élément spécifié est présent dans la liste.
+     * @param e l'élément à rechercher dans la liste.
+     * @return vrai si l'élément est présent dans la liste, faux sinon.
+     **/
+    public boolean contient(T e) {
+        Maillon maillonCourant = tete;
+        while (maillonCourant != null) {
+            if (maillonCourant.donnee.equals(e)) {
+                return true;
+            }
+            maillonCourant = maillonCourant.suivant;
+        }
+        return false;
+    }
 }
